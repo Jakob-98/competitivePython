ec8b9bfb5a32de42327fe9c4826477f7edd77b93
Jakob Serlier <37184788+Jakob-98@users.noreply.github.com>
2020-07-11 11:09:34 +0200

    Delete b1.py

--- b1.py
+++
@@ -1,9 +0,0 @@
-cases = int(input())
-for case in cases:
-    N = int(input())
-    heights = list(map(int, input().split()))
-    peaks = 0
-    for i in range(1,len(heights)-1):
-        if heights[i] > heights[i+1] and heights[i] > heights[i-1]:
-            peaks += 1
-    print('Case #' + str(case) + ": " + str(peaks))
